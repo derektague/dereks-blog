88b5777abec3b36dfd9bb6235656f11ccbee9985
derektague <derek@atwavelength.com>
2025-04-08 20:07:54 -0600

    removed images block

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: dereks-blog.vercel.app        ,
-        port: "",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
